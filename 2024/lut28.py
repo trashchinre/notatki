5a82f725afe4552c4d84f6fbb99b7b6933ba7322
jasiu <162505645+trashchinre@users.noreply.github.com>
2024-04-03 10:22:38 +0200

    t19

---
+++ 2024/lut28.py
@@ -0,0 +1,9 @@
+imie = input()
+bok_kwadratu = int(input()) # int - calkowite ; float - po przecinku liczby
+print(bok_kwadratu**2)
+a,b = input().split()
+print(a)
+a = int(a)
+print(type(a))
+imie = input().split('a')
+print(imie)
