39bffbe2984ce8ba22c43a460940b1db609747f5
jasiu <162505645+trashchinre@users.noreply.github.com>
2024-04-03 10:24:55 +0200

    t19

---
+++ 2024/lut29.py
@@ -0,0 +1,17 @@
+# definicje
+def nazwa(dane):
+#    kod
+def dodawanie(a,b):
+    return(a+b)
+
+##################################
+# szybkie powtorki
+imie='jan'
+print(imie[-1])
+a = 5
+a+=1
+print(a)
+##################################
+
+w1,p1,w2,p2 = map(int, input('Podaj dane: ').split())
+print(w1*p2+w2*p2)
